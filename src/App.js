58b0b40f8204e4afb122948dff95f3c48967da18
Jyoti Patidar <jyotipatidar20@navgurukul.org>
2022-12-22 11:19:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from "react";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App(){
+const[data,setData]=useState([])
+  const getData=async()=>{
+  const response=await fetch('https://jsonplaceholder.typicode.com/users')
+  const result =await response.json()
+  setData(result)
+  console.log(result)
 }
+useEffect(()=>{
+getData()
+},[])
+return (
+  <div>
+    <h1  style={{marginLeft:"600px"}}>Data table</h1>
+    <table>
+         <tr>
+              <th>id</th>
+              <th> companyname</th>
+              <th>username</th>
+              <th>email</th>
+              <th>city</th>
+              <th>zipcode</th>
+              {/* <th>company name</th> */}
+              <th>phone no</th>
+          </tr>
+    {
+      
+    data.map((item,index)=>{
+      return(
+          
+              <tr>
+              <td>{item.id}</td>
+              <td>{item.company. name}</td>
+              <td>{item.username}</td>
+              <td>{item.email}</td>
+              <td>{item.address.city}</td>
+              <td>{item.address.zipcode}</td>
+              {/* <td>{item.company.name}</td> */}
+              <td>{item.phone}</td>
 
-export default App;
+              </tr>
+      )
+    })
+  }
+   </table>
+  </div>
+
+)
+}
